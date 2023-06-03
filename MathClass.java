8fea877992735bb8a373b7885a9d1b0e789bccd0
Arun kumar <arun.kumar@clarivate.com>
2023-06-03 23:20:58 +0530

    added math.java file

---
+++ MathClass.java
@@ -0,0 +1,15 @@
+package com.java8;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		
+		int a =10,b=20;
+		System.out.println("add-"+add(a,b));
+	}
+
+	private static int add(int a, int b) {
+		
+		return a+b;
+	}
+}
